ac975e951da506c11be04039e6465ed08fa8476a
Randy Wressell <randy5235@hotmail.com>
2017-07-14 23:33:50 -0700

    testing;

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-const myVar = 'Test for mustache';
-
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Please open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the {myVar} developer menu.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
